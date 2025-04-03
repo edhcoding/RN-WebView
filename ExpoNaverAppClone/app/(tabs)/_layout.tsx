b37457e035fbdc1cb2b4e2546a2863efbeb9cfc6
eundonghyeok <mephisto7191@naver.com>
2025-04-03 15:22:27 +0900

    Feat: 탭 아이콘 변경

--- ExpoNaverAppClone/app/(tabs)/_layout.tsx
+++ ExpoNaverAppClone/app/(tabs)/_layout.tsx
@@ -1,11 +1,52 @@
-import { Tabs } from "expo-router/tabs";
 import React from "react";
+import { Tabs } from "expo-router/tabs";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
+const HomeIcon = ({ focused, color }: { focused: boolean; color: string }) => {
+  return (
+    <MaterialCommunityIcons
+      name={focused ? "home" : "home-outline"}
+      color={color}
+      size={26}
+    />
+  );
+};
+
+const ShoppingIcon = ({
+  focused,
+  color,
+}: {
+  focused: boolean;
+  color: string;
+}) => {
+  return (
+    <MaterialCommunityIcons
+      name={focused ? "cart" : "cart-outline"}
+      color={color}
+      size={26}
+    />
+  );
+};
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" /> {/* index 가 홈 스크린 */}
-      <Tabs.Screen name="shopping" /> {/* index 가 홈 스크린 */}
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: "black",
+        },
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "gray",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{ tabBarLabel: "홈", tabBarIcon: HomeIcon }}
+      />
+      <Tabs.Screen
+        name="shopping"
+        options={{ tabBarLabel: "쇼핑", tabBarIcon: ShoppingIcon }}
+      />
     </Tabs>
   );
 }
