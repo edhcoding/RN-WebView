c50ab1feea2da4cae8261a01e6ea5061f78c573f
eundonghyeok <mephisto7191@naver.com>
2025-04-03 16:27:04 +0900

    Feat: 네비게이션 기능(앞, 뒤, 새로고침)

--- ExpoNaverAppClone/app/browser.tsx
+++ ExpoNaverAppClone/app/browser.tsx
@@ -1,4 +1,4 @@
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import React, { useMemo, useRef, useState } from "react";
 import {
   Animated,
@@ -7,9 +7,11 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from "react-native";
 import WebView from "react-native-webview";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 const styles = StyleSheet.create({
   safearea: {
@@ -34,7 +36,57 @@ const styles = StyleSheet.create({
     height: "100%",
     backgroundColor: "green",
   },
+  navigator: {
+    backgroundColor: "black",
+    flexDirection: "row",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    justifyContent: "space-between",
+  },
+  button: {
+    width: 30,
+    height: 30,
+    padding: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  naverIconOutline: {
+    borderWidth: 1,
+    borderColor: "white",
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  naverIconText: {
+    color: "white",
+  },
 });
+
+const NavButton = ({
+  iconName,
+  disabled,
+  onPress,
+}: {
+  iconName: keyof typeof MaterialCommunityIcons.glyphMap;
+  disabled?: boolean;
+  onPress?: () => void;
+}) => {
+  return (
+    <TouchableOpacity
+      style={styles.button}
+      onPress={onPress}
+      disabled={disabled}
+    >
+      <MaterialCommunityIcons
+        name={iconName}
+        size={24}
+        color={disabled ? "gray" : "white"}
+        style={styles.naverIconText}
+      />
+    </TouchableOpacity>
+  );
+};
 
 export default function BrowserScreen() {
   // index.tsx에서 넘겨준 파라미터 사용하려면 useLocalSearchParams를 사용해야 함
@@ -51,6 +103,10 @@ export default function BrowserScreen() {
 
   // Animated.Value를 0으로 초기화해서 progressAnim에 ref로 저장
   const progressAnim = useRef(new Animated.Value(0)).current;
+  const webViewRef = useRef<WebView | null>(null);
+
+  const [canGoBack, setCanGoBack] = useState(false);
+  const [canGoForward, setCanGoForward] = useState(false);
 
   return (
     <SafeAreaView style={styles.safearea}>
@@ -71,16 +127,44 @@ export default function BrowserScreen() {
         />
       </View>
       <WebView
+        ref={webViewRef}
         source={{ uri: initialUrl as string }}
         showsVerticalScrollIndicator={false}
         showsHorizontalScrollIndicator={false}
         // 웹 페이지가 변경될 때 마다 주소 트리거 해줘야함 (현재 접속된 주소를 트리거)
-        onNavigationStateChange={(e) => setUrl(e.url)}
+        onNavigationStateChange={(e) => {
+          setUrl(e.url);
+          // 뒤로 갈 수 있는지 여부 파악 가능 (canGoBack 속성)
+          setCanGoBack(e.canGoBack);
+          setCanGoForward(e.canGoForward);
+        }}
         // webview 로딩 퍼센트
         onLoadProgress={(e) => progressAnim.setValue(e.nativeEvent.progress)}
         // 로딩 끝날때 한번 실행됨 (로딩바 없애기 위함)
         onLoadEnd={() => progressAnim.setValue(0)}
       />
+      <View style={styles.navigator}>
+        <TouchableOpacity style={styles.button} onPress={() => router.back()}>
+          <View style={styles.naverIconOutline}>
+            <Text style={styles.naverIconText}>N</Text>
+          </View>
+        </TouchableOpacity>
+        {/* 이제 웹뷰에게 뒤로 가라는 명령을 하려면 webview reference에 접근해야함 */}
+        <NavButton
+          iconName="arrow-left"
+          disabled={!canGoBack}
+          onPress={() => webViewRef.current?.goBack()}
+        />
+        <NavButton
+          iconName="arrow-right"
+          disabled={!canGoForward}
+          onPress={() => webViewRef.current?.goForward()}
+        />
+        <NavButton
+          iconName="refresh"
+          onPress={() => webViewRef.current?.reload()}
+        />
+      </View>
     </SafeAreaView>
   );
 }
