334f2f0b3f543bd8a4443a321a31ccb5a09c219e
eundonghyeok <mephisto7191@naver.com>
2025-04-10 16:00:27 +0900

    Chore: expo 초기 세팅

---
+++ ExpoAiRecorderApp/.prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  arrowParens: "avoid", // 화살표 함수에서 매개변수가 하나일 때는 괄호 생략
+  bracketSameLine: true, // 닫는 괄호 줄바꿈 제거
+  bracketSpacing: true, // 객체 리터럴에서 쉼표 뒤에 공백 추가
+  singleQuote: true,
+  trailingComma: "all", // 항상 마지막에 , 콤마 붙이기
+};
