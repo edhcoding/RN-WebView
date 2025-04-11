b90c68b9fbee45a50eca37dc89a9a448098310c6
eundonghyeok <mephisto7191@naver.com>
2025-04-11 14:37:36 +0900

    Feat: 웹앱에서 사진 첨부 기능 구현

--- ExpoAiRecorderApp/App.tsx
+++ ExpoAiRecorderApp/App.tsx
@@ -1,23 +1,62 @@
 import { Audio } from 'expo-av';
 import { RecordingOptionsPresets } from 'expo-av/build/Audio';
 import React, { useCallback, useRef, useState } from 'react';
-import { Platform, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import WebView from 'react-native-webview';
 import * as FileSystem from 'expo-file-system'; // 파일 시스템 import 이렇게 해줘야함
+import { CameraView, useCameraPermissions } from 'expo-camera';
 
 const styles = StyleSheet.create({
   safearea: {
     flex: 1,
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
+  camera: {
+    backgroundColor: 'black',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  cameraCloseButton: {
+    position: 'absolute',
+    top: 60,
+    right: 20,
+  },
+  cameraCloseText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  cameraPhotoButton: {
+    position: 'absolute',
+    width: 80,
+    height: 80,
+    borderRadius: 80 / 2,
+    backgroundColor: 'white',
+    bottom: 60,
+    alignSelf: 'center',
+  },
 });
 
 export default function App() {
   const webViewRef = useRef<WebView | null>(null);
+  const cameraRef = useRef<CameraView | null>(null);
   const [audioPermissionResponse, requestAudioPermission] =
     Audio.usePermissions();
+  const [cameraPermissionResponse, requestCameraPermission] =
+    useCameraPermissions();
 
   const [recording, setRecording] = useState<Audio.Recording | null>();
+  const [isCameraOn, setIsCameraOn] = useState<boolean>(false);
 
   const sendMessageToWebView = useCallback(
     ({ type, data }: { type: string; data?: any }) => {
@@ -87,6 +126,35 @@ export default function App() {
     sendMessageToWebView({ type: 'onResumeRecord' });
   }, [recording, sendMessageToWebView]);
 
+  const openCamera = useCallback(async () => {
+    const response = await requestCameraPermission();
+
+    if (response.granted) {
+      setIsCameraOn(true);
+    }
+  }, [requestCameraPermission]);
+
+  const closeCamera = useCallback(() => {
+    setIsCameraOn(false);
+  }, []);
+
+  const onPressPhotoButton = useCallback(async () => {
+    const picture = await cameraRef.current?.takePictureAsync({ quality: 0 });
+
+    if (picture?.uri != null) {
+      const base64Image = await FileSystem.readAsStringAsync(picture.uri, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+      // base64만 보내는게 아니고 이미지 타입도 같이 보내줘야 웹에서 이미지로 보여줄 수 있음
+      const imageDataUrl = `data:image/jpeg:base64,${base64Image}`;
+
+      sendMessageToWebView({
+        type: 'onTakePhoto',
+        data: imageDataUrl,
+      });
+    }
+  }, [sendMessageToWebView]);
+
   return (
     <SafeAreaView style={styles.safearea}>
       <WebView
@@ -103,9 +171,29 @@ export default function App() {
             pauseRecord();
           } else if (type === 'resume-record') {
             resumeRecord();
+          } else if (type === 'open-camera') {
+            openCamera();
           }
         }}
       />
+      {isCameraOn && (
+        <View style={styles.camera}>
+          <CameraView
+            ref={cameraRef}
+            style={StyleSheet.absoluteFill}
+            facing="back">
+            <TouchableOpacity
+              style={styles.cameraCloseButton}
+              onPress={closeCamera}>
+              <Text style={styles.cameraCloseText}>CLOSE</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.cameraPhotoButton}
+              onPress={onPressPhotoButton}
+            />
+          </CameraView>
+        </View>
+      )}
     </SafeAreaView>
   );
 }
